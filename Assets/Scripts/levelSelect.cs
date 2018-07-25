bb9aaf08284292a49d764a0a922e931ca6d6ea9a
spamTeam <41636481+spamTeam@users.noreply.github.com>
2018-07-25 20:11:04 +0200

    add scene mainmenu

--- Assets/Scripts/levelSelect.cs
+++ Assets/Scripts/levelSelect.cs
@@ -6,18 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class levelSelect : MonoBehaviour {
 
-    public Animator transitionAnim;
-    public string sceneName;
-    public KeyCode keypress;
+    public Animator transitionAnim1;
+    public Animator transitionAnim2;
+    public string sceneName1;
+    public string sceneName2;
+    public KeyCode keypresslevel1;
+    public KeyCode keypresslevel2;
 
     //Fonction pour géré l'animation et le passage à la scène suivante
-    IEnumerator LoadScene()
+    IEnumerator LoadScene1()
     {
-        transitionAnim.SetTrigger("level1trig");
+        transitionAnim1.SetTrigger("level1trig");
         yield return new WaitForSeconds(1.0f);
-        SceneManager.LoadScene("level1");
+        SceneManager.LoadScene(sceneName1);
     }
 
+    IEnumerator LoadScene2()
+    {
+        transitionAnim2.SetTrigger("level1trig");
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(sceneName2);
+    }
 
     // Use this for initialization
     void Start () {
@@ -27,9 +36,13 @@ public class levelSelect : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         
-        if(Input.GetKeyDown(keypress))
+        if(Input.GetKeyDown(keypresslevel1))
         {
-            StartCoroutine(LoadScene());
+            StartCoroutine(LoadScene1()); //appel de la fonction
+        }
+        else if (Input.GetKeyDown(keypresslevel2))
+        {
+            StartCoroutine(LoadScene2()); //appel de la fonction
         }
 
         
