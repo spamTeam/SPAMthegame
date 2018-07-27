a1d7939ca90ea5a6000dcbe7459a8cec5d4952d9
Hugo <41636481+spamTeam@users.noreply.github.com>
2018-07-27 14:26:36 +0200

    update level

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicController : MonoBehaviour {
+
+    AudioSource music;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+        music = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (SceneManager.GetActiveScene().name.Equals("Mainmenu") || SceneManager.GetActiveScene().name.Equals("EndWorld1")) { music.Stop(); }
+	}
+}
