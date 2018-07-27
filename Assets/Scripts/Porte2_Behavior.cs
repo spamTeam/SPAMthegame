56756c463e5c1f07e45924a51512f42ee1c14485
Hugo <41636481+spamTeam@users.noreply.github.com>
2018-07-27 15:54:40 +0200

    premier monde + changement porte2

--- Assets/Scripts/Porte2_Behavior.cs
+++ Assets/Scripts/Porte2_Behavior.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Porte2_Behavior : MonoBehaviour {
 
-    public Vector2 destination_Horizontal1;
-    public Vector2 destination_Vertical1;
-    public Vector2 destination_Horizontal2;
-    public Vector2 destination_Vertical2;
-    public Vector2 destination_Horizontal3;
-    public Vector2 destination_Vertical3;
-    public Vector2 destination_Horizontal4;
-    public Vector2 destination_Vertical4;
+    public Vector2 Horizontal1;
+    public Vector2 Vertical1;
+    public Vector2 Horizontal2;
+    public Vector2 Vertical2;
+    public Vector2 Horizontal3;
+    public Vector2 Vertical3;
+    public Vector2 Horizontal4;
+    public Vector2 Vertical4;
+
     public Button_Behavior ButtonBehavior;
     private double iOnOff;
     public float SensRotation;
@@ -19,7 +20,7 @@ public class Porte2_Behavior : MonoBehaviour {
     private double modOnOff;
     private double mod2OnOff;
 
-    private bool InCollision = false;
+    private bool InCollision;
     private string direction;
     private Vector3 CollisionPos;
     private Vector3 CurrentPos;
@@ -36,10 +37,24 @@ public class Porte2_Behavior : MonoBehaviour {
         modOnOff = 0;
         iOnOffLast = 0;
         countrotation = 0;
+        InCollision = false;
     }
 
     // Update is called once per frame
     void Update () {
+
+        if (InCollision)
+        {
+            if (Mathf.Abs(CollisionPos.x - CurrentPos.x) > Mathf.Abs(CollisionPos.y - CurrentPos.y))
+            {
+                direction = "Horizontal";
+            }
+            else if (Mathf.Abs(CollisionPos.x - CurrentPos.x) < Mathf.Abs(CollisionPos.y - CurrentPos.y))
+            {
+                direction = "Vertical";
+            }
+            else { Debug.Log("CollisionPos.x - CurrentPos.x = CollisionPos.y - CurrentPos.y"); }
+        }
 
         //// Gesion des animation
         iOnOffTOT = ButtonBehavior.OnOff();
@@ -47,7 +62,7 @@ public class Porte2_Behavior : MonoBehaviour {
         {
             iOnOffLast = iOnOffTOT;
 
-            if (destination_Horizontal1 == destination_Horizontal3 && destination_Horizontal2 == destination_Horizontal4 && destination_Vertical1 == destination_Vertical3 && destination_Vertical2 == destination_Vertical4) //test si on a un interupteur 2 ou 4 position
+            if (Horizontal1 == Horizontal3 && Horizontal2 == Horizontal4 && Vertical1 == Vertical3 && Vertical2 == Vertical4) //test si on a un interupteur 2 ou 4 position
             {
                 if (countrotation == 0)
                 {
@@ -65,6 +80,7 @@ public class Porte2_Behavior : MonoBehaviour {
             }
             else
             {
+
                 if (countrotation == 0)
                 {
                     countrotation = 1;
@@ -96,38 +112,25 @@ public class Porte2_Behavior : MonoBehaviour {
 
             }
 
-        }
-
-        ////FIN animation ////
 
 
-        if (InCollision) {
-            if (Mathf.Abs(CollisionPos.x-CurrentPos.x) > Mathf.Abs(CollisionPos.y - CurrentPos.y))
-            {
-                direction = "Horizontal";
-            }
-            else if(Mathf.Abs(CollisionPos.x - CurrentPos.x) < Mathf.Abs(CollisionPos.y - CurrentPos.y))
-            {
-                direction = "Vertical";
-            }
-            else { Debug.Log("CollisionPos.x - CurrentPos.x = CollisionPos.y - CurrentPos.y"); }
-        }
+            ////FIN animation ////
 
-        if (ButtonBehavior != null)
-        {
-            iOnOff = ButtonBehavior.OnOff();
-            modOnOff = iOnOff % 4;
-            if (destination_Horizontal1 == destination_Horizontal3 && destination_Horizontal2 == destination_Horizontal4 && destination_Vertical1 == destination_Vertical3 && destination_Vertical2 == destination_Vertical4)
+            if (ButtonBehavior != null)
             {
-                mod2OnOff = iOnOff % 2;
-                transform.rotation = Quaternion.EulerAngles(new Vector3(0, 0, startAngle - Mathf.PI / 2 * (SensRotation * (float)mod2OnOff)));
-            }
-            else
-            {
-                transform.rotation = Quaternion.EulerAngles(new Vector3(0, 0, startAngle - Mathf.PI / 2 * (SensRotation * (float)modOnOff)));
+                iOnOff = ButtonBehavior.OnOff();
+                modOnOff = iOnOff % 4;
+                if (Horizontal1 == Horizontal3 && Horizontal2 == Horizontal4 && Vertical1 == Vertical3 && Vertical2 == Vertical4)
+                {
+                    mod2OnOff = iOnOff % 2;
+                    transform.rotation = Quaternion.EulerAngles(new Vector3(0, 0, startAngle - Mathf.PI / 2 * (SensRotation * (float)mod2OnOff)));
+                }
+                else
+                {
+                    transform.rotation = Quaternion.EulerAngles(new Vector3(0, 0, startAngle - Mathf.PI / 2 * (SensRotation * (float)modOnOff)));
+                }
             }
         }
-
 
     }
 
@@ -151,28 +154,30 @@ public class Porte2_Behavior : MonoBehaviour {
 
     public Vector3 GetNewDestination()
     {
+        Debug.Log(direction);
+
         if (modOnOff == 0)
         {
-            if (direction == "Horizontal") { return destination_Vertical1; }
-            else if (direction == "Vertical") { return destination_Horizontal1; }
+            if (direction == "Vertical") { return Vertical1; }
+            else if (direction == "Horizontal") { return Horizontal1; }
             else { return Vector2.zero; }
         }
         else if (modOnOff == 1)
         {
-            if (direction == "Horizontal") { return destination_Vertical2; }
-            else if (direction == "Vertical") { return destination_Horizontal2; }
+            if (direction == "Vertical") { return Vertical2; }
+            else if (direction == "Horizontal") { return Horizontal2; }
             else { return Vector2.zero; }
         }
         else if (modOnOff == 2)
         {
-            if (direction == "Horizontal") { return destination_Vertical3; }
-            else if (direction == "Vertical") { return destination_Horizontal3; }
+            if (direction == "Vertical") { return Vertical3; }
+            else if (direction == "Horizontal") { return Horizontal3; }
             else { return Vector2.zero; }
         }
         else if (modOnOff == 3)
         {
-            if (direction == "Horizontal") { return destination_Vertical4; }
-            else if (direction == "Vertical") { return destination_Horizontal4; }
+            if (direction == "Vertical") { return Vertical4; }
+            else if (direction == "Horizontal") { return Horizontal4; }
             else { return Vector2.zero; }
         }
         else { return Vector2.zero; }
